c63a8e1573cee61b13302c932082801c7116abae
uneoka shun <nodoubtnolife1109@gmail.com>
2024-10-14 11:25:21 +0900

    first commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/search', [ProductController::class, 'search'])->name('products.search');
-Route::get('/products/register', [ProductController::class, 'create'])->name('products.create');
-Route::get('/products/{product}/update', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::delete('/products/{product}/delete', [ProductController::class, 'destroy'])->name('products.destroy');
